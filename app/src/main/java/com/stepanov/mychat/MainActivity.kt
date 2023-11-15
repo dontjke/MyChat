34f173e4c0545842777d7a42c9d82e2bbed49194
Romajke <skuaall@mail.ru>
2023-11-15 18:11:29 +0300

    added simple interface for sending and display

--- app/src/main/java/com/stepanov/mychat/MainActivity.kt
+++ app/src/main/java/com/stepanov/mychat/MainActivity.kt
@@ -3,18 +3,45 @@ package com.stepanov.mychat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.Firebase
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.database
+import com.stepanov.mychat.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // Write a message to the database
         val database = Firebase.database
         val myRef = database.getReference("message")
 
-        myRef.setValue("Hello, World!!!")
+        binding.sendButton.setOnClickListener {
+            myRef.setValue(binding.textMessage.text.toString())
+        }
+        onChangeListener(myRef)
+    }
+
+    private fun onChangeListener(databaseReference: DatabaseReference){
+        databaseReference.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                binding.apply {
+                    recyclerView.append("\n")
+                    recyclerView.append("Roman: ${snapshot.value.toString()}")
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
     }
 }
